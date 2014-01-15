e2afd2412eb4c0ceace6e40b1cd9ad63dadbed35
Elizabeth Clarke <literarylizbeth@gmail.com>
2014-01-15 14:19:27 -0600

    switched to simple_form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def bootstrap_form_settings
+     { wrapper_html: { class: "form-group" }, 
+       input_html: { class: "form-control" },
+       required: false }
+  end
+
 end
